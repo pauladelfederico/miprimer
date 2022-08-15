8e99552c3600451ac9b0e311434d1e569551bd94
Jigus <jiv.bio@gmail.com>
2022-08-14 21:55:44 -0300

    agrego archivo .py ejerc 3 Introduccion a la Programacion

---
+++ lista3.py
@@ -0,0 +1,9 @@
+id_perro=13
+nombre="Puppy"
+fecha_de_nacimiento="12/12/2012"
+sexo="Macho"
+dni=123
+perro=[id_perro,nombre,fecha_de_nacimiento, sexo, dni]
+perro[2]="13/12/2012"
+perro[4]=28957346
+print(perro)
