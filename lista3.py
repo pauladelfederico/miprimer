5be8c7f1d1018fbc317f210a869d2bfcc3daf441
Jimena V <99764302+jivdev@users.noreply.github.com>
2022-08-14 22:26:35 -0300

    Delete lista3.py

--- lista3.py
+++
@@ -1,9 +0,0 @@
-id_perro=13
-nombre="Puppy"
-fecha_de_nacimiento="12/12/2012"
-sexo="Macho"
-dni=123
-perro=[id_perro,nombre,fecha_de_nacimiento, sexo, dni]
-perro[2]="13/12/2012"
-perro[4]=28957346
-print(perro)
